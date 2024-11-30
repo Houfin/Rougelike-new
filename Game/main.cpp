8697f41eb205d54b49cdc0ff5ffa89bfaa43e5f3
Houfin64 <67628989+Houfin64@users.noreply.github.com>
2024-11-30 17:42:59 +0000

    Added Movement and solid screen edges

--- Game/main.cpp
+++ Game/main.cpp
@@ -2,14 +2,16 @@
 #include <iostream> 
 #include <stdio.h>
 
-int main() {
-    // Creates a window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "CPP Rougelike");
-
-    if (!window.isOpen()) {
-        std::cerr << "Window couldn't be opened!" << std::endl;
-        return -1;  // Exit if the window fails to open
+void moveEntity(sf::Text* entity, int xChange, int yChange) {
+    float tileWidth = entity->getLocalBounds().width;
+    float tileHeight = entity->getLocalBounds().height;
+    if (entity->getPosition().x + xChange < 0 || entity->getPosition().x + xChange > 80*tileWidth || entity->getPosition().y + yChange < 0 || entity->getPosition().y + yChange > 50 * tileHeight) {
+        return;
     }
+    entity->setPosition(entity->getPosition().x + xChange, entity->getPosition().y + yChange);
+}
+
+int main() {
 
     //Loads a font from a file
     sf::Font font;
@@ -18,17 +20,27 @@ int main() {
         return -1;
     }
 
-    sf::Text text;
-    text.setFont(font);
-    text.setString("@");
-    text.setCharacterSize(24);
-    text.setFillColor(sf::Color::White);
+    //Creates the player sprite
+    sf::Text playerText;
+    playerText.setFont(font);
+    playerText.setString("@");
+    playerText.setCharacterSize(24);
+    playerText.setFillColor(sf::Color::White);
 
-    sf::FloatRect textbounds = text.getLocalBounds();
-    float textWidth = textbounds.width;
-    float textHeight = textbounds.height;
+    sf::FloatRect textbounds = playerText.getLocalBounds();
+    float tileWidth = textbounds.width;
+    float tileHeight = textbounds.height;
 
-    text.setPosition((window.getSize().x - textWidth) / 2, (window.getSize().y - textHeight) / 2);
+
+    // Creates a window
+    sf::RenderWindow window(sf::VideoMode(tileWidth*82, tileHeight*52), "CPP Rougelike");
+
+    if (!window.isOpen()) {
+        std::cerr << "Window couldn't be opened!" << std::endl;
+        return -1;  // Exit if the window fails to open
+    }
+
+    playerText.setPosition(tileWidth*40, tileHeight*25);
 
     // Game loop
     while (window.isOpen()) {
@@ -37,12 +49,43 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) //red x in the corner is pressed
                 window.close();
+
+            //Check for all relevant keypress events
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Up || event.key.code == sf::Keyboard::Numpad8) {
+                    moveEntity(&playerText, 0, -tileHeight);
+                }
+                if (event.key.code == sf::Keyboard::Down || event.key.code == sf::Keyboard::Numpad2) {
+                    moveEntity(&playerText, 0, tileHeight);
+                }
+                if (event.key.code == sf::Keyboard::Left || event.key.code == sf::Keyboard::Numpad4) {
+                    moveEntity(&playerText, -tileWidth, 0);
+                }
+                if (event.key.code == sf::Keyboard::Right || event.key.code == sf::Keyboard::Numpad6) {
+                    moveEntity(&playerText, tileWidth, 0);
+                }
+                if (event.key.code == sf::Keyboard::Numpad7) {
+                    moveEntity(&playerText, -tileWidth, -tileHeight);
+                }
+                if (event.key.code == sf::Keyboard::Numpad9) {
+                    moveEntity(&playerText, tileWidth, -tileHeight);
+                }
+                if (event.key.code == sf::Keyboard::Numpad1) {
+                    moveEntity(&playerText, -tileWidth, tileHeight);
+                }
+                if (event.key.code == sf::Keyboard::Numpad3) {
+                    moveEntity(&playerText, tileWidth, tileHeight);
+                }
+                if (event.key.code == sf::Keyboard::Escape) {
+                    window.close();
+                }
+            }
         }
 
         // Clear the window with a black color
         window.clear(sf::Color::Black);
 
-        window.draw(text);
+        window.draw(playerText);
 
         // Display the contents of the window
         window.display();
